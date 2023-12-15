da04f24b1c6790bee0cc4ce87884585cb158c5e3
Darley Araújo Silva <darley.silva@funttastic.com>
2023-12-14 23:13:15 -0300

    Bringing back the condition that was removed by mistake.

--- all/install.sh
+++ all/install.sh
@@ -619,7 +619,10 @@ post_installation_fun_hb_client () {
   docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c "cd resources && chown -RH :$GROUP ."
   docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c "cd resources && chmod -R a+rwX ."
   docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c "python app.py" > /dev/null 2>&1 &
-  docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c 'echo "$(cat selected_passphrase.txt)" > resources/random_passphrase.txt' &> /dev/null
+
+  if [ -n "$RANDOM_PASSPHRASE" ]; then
+    docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c 'echo "$(cat selected_passphrase.txt)" > resources/random_passphrase.txt' &> /dev/null
+  fi
 
   docker exec "$FUN_HB_CLIENT_CONTAINER_NAME" /bin/bash -c "rm -f selected_passphrase.txt"
 }
