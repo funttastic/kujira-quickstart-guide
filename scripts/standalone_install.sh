51f9cff5cebca56d5a0714b279fdf4884ff84d45
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-07-29 13:56:23 -0300

    Fixing standalone_install.sh script.

--- scripts/standalone_install.sh
+++ scripts/standalone_install.sh
@@ -1423,7 +1423,7 @@ replace_in_file() {
 	local search_regex="$2"
 	local substitution_regex="$3"
 
-	 python functions.py "$function_name" "$file_path" "$search_regex" "$substitution_regex"
+	 python /home/user/shared/scripts/functions.py "$function_name" "$file_path" "$search_regex" "$substitution_regex"
 }
 
 replace_environment_variable() {
@@ -1432,7 +1432,7 @@ replace_environment_variable() {
 	local variable_name="$2"
 	local new_value="$3"
 
-	python functions.py "$function_name" "$file_path" "$variable_name" "$new_value"
+	python /home/user/shared/scripts/functions.py "$function_name" "$file_path" "$variable_name" "$new_value"
 }
 
 change_user_and_password() {
